a202a5c0f9d5941edc9722330331d50483b9076a
Caroline Grimes <grimes.caroline@gmail.com>
2013-10-27 14:29:51 -0400

    Refactor NegativeDescriptorState

--- app/models/automated_metareview/state.rb
+++ app/models/automated_metareview/state.rb
@@ -155,36 +155,53 @@ class SuggestiveState < State
   end
 end
 class NegativeDescriptorState < State
+  @state
+  @prev_negative_word
+  @interim_noun_verb
+
   def next_state(current_token_type, prev_negative_word, interim_noun_verb)
-    state = get_state()
-    if(current_token_type == NEGATIVE_WORD)
-      if(interim_noun_verb == true)#there are some words in between
-        state = NEGATIVE_WORD #e.g: "hard(-) to understand none(-) of the comments"
-      else
-        state = POSITIVE #e.g."He hardly not...."
-      end
-      interim_noun_verb = false #resetting
-    elsif(current_token_type == NEGATIVE_DESCRIPTOR)
-      if(interim_noun_verb == true)#there are some words in between
-        state = NEGATIVE_DESCRIPTOR #e.g:"there is barely any code duplication"
-      else
-        state = POSITIVE #e.g."It is hardly confusing..", but what about "it is a little confusing.."
-      end
-      interim_noun_verb = false #resetting
-    elsif(current_token_type == NEGATIVE_PHRASE)
-      if(interim_noun_verb == true)#there are some words in between
-        state = NEGATIVE_PHRASE #e.g:"there is barely any code duplication"
-      else
-        state = POSITIVE #e.g.:"it is hard and appears to be taken from"
-      end
-                                #interim_noun_verb = false #resetting
-    elsif(current_token_type == SUGGESTIVE)
-      state = SUGGESTIVE #e.g.:"I hardly(-) suggested(S) ..."
+    @prev_negative_word = prev_negative_word
+    @interim_noun_verb = interim_noun_verb
+    method = {POSITIVE => self.method(:positive), NEGATIVE_DESCRIPTOR => self.method(:negative_descriptor), NEGATIVE_PHRASE => self.method(:negative_phrase), SUGGESTIVE => self.method(:suggestive), NEGATIVE_WORD => self.method(:negative_word)}[current_token_type]
 
+    method.call()
+
+    @interim_noun_verb = false #resetting
+
+    return @state, @interim_noun_verb
+
+  end
+  def negative_word
+    if(@interim_noun_verb == true)#there are some words in between
+      @state = NEGATIVE_WORD #e.g: "hard(-) to understand none(-) of the comments"
+    else
+      @state = POSITIVE #e.g."He hardly not...."
     end
-    interim_noun_verb = false #resetting
-
-    return state, interim_noun_verb
+    puts "next token is negative"
+  end
+  def positive
+    @state = get_state()
+    puts "next token is positive"
+  end
+  def negative_descriptor
+    if(@interim_noun_verb == true)#there are some words in between
+      @state = NEGATIVE_DESCRIPTOR #e.g:"there is barely any code duplication"
+    else
+      @state = POSITIVE #e.g."It is hardly confusing..", but what about "it is a little confusing.."
+    end
+    puts "next token is negative"
+  end
+  def negative_phrase
+    if(@interim_noun_verb == true)#there are some words in between
+      @state = NEGATIVE_PHRASE #e.g:"there is barely any code duplication"
+    else
+      @state = POSITIVE #e.g.:"it is hard and appears to be taken from"
+    end
+    puts "next token is negative phrase"
+  end
+  def suggestive
+    @state = SUGGESTIVE #e.g.:"I hardly(-) suggested(S) ..."
+    puts "next token is suggestive"
   end
   def get_state
     puts "negative_descriptor"
